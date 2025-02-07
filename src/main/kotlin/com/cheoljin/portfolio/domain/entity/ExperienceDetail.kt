257350955ca7e145d27b98218370174f49773383
tteks149 <tteks149@naver.com>
2025-02-07 20:05:48 +0900

    앤티티개발 -연관관계 있음

--- src/main/kotlin/com/cheoljin/portfolio/domain/entity/ExperienceDetail.kt
+++ src/main/kotlin/com/cheoljin/portfolio/domain/entity/ExperienceDetail.kt
@@ -3,9 +3,19 @@ package com.cheoljin.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class ExperienceDetail : BaseEntity() {
+class ExperienceDetail(content: String, isActive: Boolean) : BaseEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "experience_detail_id")
     var id: Long? = null // ?는 null 허용
+
+    var content: String = content
+    var isActive: Boolean = isActive
+
+    fun update(content:String, isActive: Boolean){
+        this.content = content
+        this.isActive = isActive
+    }
+
+
 }
--- src/main/kotlin/com/cheoljin/portfolio/domain/entity/ProjectDetail.kt
+++ src/main/kotlin/com/cheoljin/portfolio/domain/entity/ProjectDetail.kt
@@ -3,9 +3,22 @@ package com.cheoljin.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class ProjectDetail : BaseEntity() {
+class ProjectDetail(content: String, url:String?, isActive: Boolean) : BaseEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "project_detail_id")
     var id: Long? = null // ?는 null 허용
+
+    var content: String = content
+    var url : String? = url
+    var isActive: Boolean = isActive
+
+    fun update(content:String, url:String?,isActive: Boolean){
+        this.content = content
+        this.isActive = isActive
+        this.url = url
+    }
+
+
+
 }
